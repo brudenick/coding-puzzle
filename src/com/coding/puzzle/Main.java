70600deaa7126c8514125850196e25bc48af4477
Bruno Rudenick <bruno.rudenick@inallmedia.com>
2019-07-05 12:29:33 -0300

    Initial Commit

---
+++ src/com/coding/puzzle/Main.java
@@ -0,0 +1,9 @@
+package com.coding.puzzle;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+
+}
---
+++ src/com/coding/puzzle/model/Character.java
@@ -0,0 +1,3 @@
+package com.coding.puzzle.model;
+
+public class Character {}
